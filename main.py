8283bbb466d77cd0646e559c78e91c32e79ab6d1
Ania Misiorek <amisiore@akamai.com>
2023-09-13 12:50:46 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from metadata import MetadataClient
+
+
+def main():
+    client = MetadataClient()
+    # All of these responses are handled as DataClasses,
+    # allowing IDEs to properly use completions.
+    instance_info = client.get_instance()
+    network_info = client.get_network()
+    ssh_info = client.get_ssh_keys()
+    user_data = client.get_user_data()
+    print("Instance ID:", instance_info.instance_id)
+    print("Public IPv4:", network_info.ipv4.public[0])
+    print("SSH Keys:", "; ".join(ssh_info.users.root))
+    print("User Data:", user_data)
+
+
+if __name__ == "__main__":
+    main()
