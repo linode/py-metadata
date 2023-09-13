366e85b3bdb8aa8f86e58028991bfad768323ab4
Ania Misiorek <amisiore@akamai.com>
2023-09-13 16:35:21 -0400

    error again

--- metadata/metadata_client.py
+++ metadata/metadata_client.py
@@ -96,7 +96,7 @@ class MetadataClient:
             except:
                 pass
 
-            print(error_msg)
+            print(resp)
             raise ApiError(error_msg, status=resp.status_code, json=j)
 
         return self._parse_response_body(resp, content_type)
