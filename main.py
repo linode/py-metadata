6579d110238c8efe5b44336b5afd6f985baa0e75
Ania Misiorek <amisiore@akamai.com>
2023-09-14 14:08:40 -0400

    adding new line for readability atm

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ def main():
     network_info = client.get_network()
     ssh_info = client.get_ssh_keys()
     user_data = client.get_user_data()
-    print("Instance ID:", instance_info)
-    print("Public IPv4:", network_info)
-    print("SSH Keys:", "; ".join(ssh_info.users.root))
-    #print("SSH Keys:", "; ", ssh_info.users)
-    print("User Data:", user_data)
+    print("Instance Info:", instance_info, "\n")
+    print("Network Info:", network_info, "\n")
+    print("SSH Keys:", "; ".join(ssh_info.users.root), "\n")
+    print("User Data:", "\n", user_data)
 
 
 if __name__ == "__main__":
