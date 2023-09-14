4e6d53249d317416e21f493895dbaf06e77971ee
Ania Misiorek <amisiore@akamai.com>
2023-09-14 09:32:58 -0400

    adding more to main.py

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def main():
     network_info = client.get_network()
     ssh_info = client.get_ssh_keys()
     user_data = client.get_user_data()
-    print("Instance ID:", instance_info.instance_id)
+    print("Instance ID:", instance_info)
     print("Public IPv4:", network_info.ipv4.public[0])
-    print("SSH Keys:", "; ".join(ssh_info.users.root))
+    #print("SSH Keys:", "; ".join(ssh_info.users.root))
+    print("SSH Keys:", "; ", ssh_info.users)
     print("User Data:", user_data)
 
 
