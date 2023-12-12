b9215ca6f5427c6ec522dd4a534bc3b10445a649
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-12-12 16:16:56 -0500

    test: Harden E2E test playbook (#11)
    * Initial hardening
    
    * Fix tests
    
    * Add README testing section
    
    * Disable root login

--- test/integration/test_ssh.py
+++ test/integration/test_ssh.py
@@ -1,11 +1,20 @@
 import re
 
+import pytest
+
 from linode_metadata.objects.ssh_keys import SSHKeysUser
 
 
 def test_get_ssh_keys(metadata_client):
     ssh = metadata_client.get_ssh_keys()
     assert isinstance(ssh.users, SSHKeysUser)
+
+    # In some cases we may not have an authorized key
+    # configured for root
+    if ssh.users.root is None:
+        pytest.skip(
+            "Skipping due to no authorized keys configured for root user"
+        )
 
     for key in ssh.users.root:
         assert isinstance(key, str)
