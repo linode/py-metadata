ffac13c9ccb5de93a5659d8e1576f04c17901fbc
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-12-18 15:24:54 -0500

    new: Implement managed token system (#16)
    * Implementation
    
    * Fix tests
    
    * Small adjustments
    
    * Add token property method
    
    * make format
    
    * Reorder _api_call pre-request logic

--- linode_metadata/metadata_client.py
+++ linode_metadata/metadata_client.py
@@ -5,7 +5,7 @@ It includes methods for retrieving and updating metadata information.
 import base64
 import datetime
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from importlib.metadata import version
 from typing import Any, Union
 
@@ -38,10 +38,12 @@ class MetadataClient:
         base_url="http://169.254.169.254/v1",
         user_agent=None,
         token=None,
-        init_token=True,
+        managed_token=True,
+        managed_token_expiry_seconds=3600,
     ):
         """
         The main interface to the Linode Metadata Service.
+
         :param base_url: The base URL for Metadata API requests.  Generally, you shouldn't
                          change this.
         :type base_url: str
@@ -51,17 +53,38 @@ class MetadataClient:
                            application.  Setting this is not necessary, but some
                            applications may desire this behavior.
         :type user_agent: str
+        :param token: An existing token to use with this client. This field cannot
+                      be specified when token management is enabled.
+        :type token: Optional[str]
+        :param managed_token: If true, the token for this client will be automatically
+                              generated and refreshed.
+        :type managed_token: bool
+        :type managed_token_expiry_seconds: The number of seconds until a managed token
+                                            should expire. (Default 3600)
+        :type managed_token_expiry_seconds: int
         """
+
+        if token is not None and managed_token:
+            raise ValueError(
+                "Token cannot be specified with token management is enabled"
+            )
 
         self.base_url = base_url
         self.session = requests.Session()
         self._append_user_agent = user_agent
+
         self._token = token
+
+        self._managed_token = managed_token
+        self._managed_token_expiry_seconds = managed_token_expiry_seconds
+        self._managed_token_expiry = None
 
         self.check_connection()
 
-        if init_token:
-            self.refresh_token()
+        if managed_token:
+            self.refresh_token(
+                expiry_seconds=self._managed_token_expiry_seconds,
+            )
 
     @property
     def _user_agent(self):
@@ -95,10 +118,21 @@ class MetadataClient:
         additional_headers=None,
         authenticated=True,
     ) -> Union[str, dict]:
-        if authenticated and self._token is None:
-            raise RuntimeError(
-                "No token provided. Please use MetadataClient.refresh_token() to create new token."
-            )
+        if authenticated:
+            # We should implicitly refresh the token if the user is enrolled in
+            # token management and the token has expired.
+            if self._managed_token and (
+                self._token is None
+                or datetime.now() > self._managed_token_expiry
+            ):
+                self.refresh_token(
+                    expiry_seconds=self._managed_token_expiry_seconds
+                )
+
+            if self._token is None:
+                raise RuntimeError(
+                    "No token provided. Please use MetadataClient.refresh_token() to create new token."
+                )
 
         method_map = {
             "GET": self.session.get,
@@ -167,7 +201,12 @@ class MetadataClient:
     def generate_token(self, expiry_seconds=3600) -> MetadataToken:
         """
         Generates a token for accessing Metadata Service.
+        NOTE: The generated token will NOT be implicitly
+        applied to the MetadataClient.
         """
+
+        created = datetime.now()
+
         resp = self._api_call(
             "PUT",
             "/token",
@@ -179,21 +218,35 @@ class MetadataClient:
         )
 
         return MetadataToken(
-            token=resp, expiry_seconds=expiry_seconds, created=datetime.now()
+            token=resp, expiry_seconds=expiry_seconds, created=created
         )
 
-    def refresh_token(self, expiry_seconds: int = 3600):
+    def refresh_token(self, expiry_seconds: int = 3600) -> MetadataToken:
         """
         Regenerates a Metadata Service token.
         """
+
         result = self.generate_token(expiry_seconds=expiry_seconds)
+
         self.set_token(result.token)
+        self._managed_token_expiry = result.created + timedelta(
+            seconds=expiry_seconds
+        )
+
+        return result
 
     def set_token(self, token: str):
         """
         Sets the passed token as token for client.
         """
         self._token = token
+
+    @property
+    def token(self):
+        """
+        Gets the token currently used by this client.
+        """
+        return self._token
 
     def get_user_data(self) -> str:
         """
--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -3,7 +3,15 @@ import pytest
 import linode_metadata
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def metadata_client():
     client = linode_metadata.MetadataClient()
+    return client
+
+
+@pytest.fixture(scope="function")
+def metadata_client_unmanaged():
+    client = linode_metadata.MetadataClient(
+        managed_token=False,
+    )
     return client
